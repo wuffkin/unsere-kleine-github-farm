47bad85d41336e3e78236efab607f63712fd6ad5
timo-cingoez <timo-cingoez@hotmail.de>
2022-07-04 15:01:33 +0200

    added println

--- app/src/main/java/com/example/unserekleinegithubfarm/MainActivity.kt
+++ app/src/main/java/com/example/unserekleinegithubfarm/MainActivity.kt
@@ -42,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        println("Timo's Änderung!")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
