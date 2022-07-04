532884504420ede9f92ebd71c162ef57133dc9d3
wuffkin <belark@gmx.de>
2022-07-04 16:02:07 +0200

    Namen geändert in value

--- app/src/main/java/com/example/unserekleinegithubfarm/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/unserekleinegithubfarm/ui/gallery/GalleryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Alious Place"
+        value = "Alious Place123"
     }
     val text: LiveData<String> = _text
 }
